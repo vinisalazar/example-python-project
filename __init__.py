8081aba3aca6440fa27cfbb2dcaf3917bc0875b3
Vini Salazar <17276653+vinisalazar@users.noreply.github.com>
2021-05-26 13:20:05 -0300

    __init__.py: add package init file.

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from .functions import (
+    load_gapminder_all,
+    create_df_views,
+    plot_gdp_50_years_diff_by_continent,
+    plot_gdp_growth_by_continent,
+    plot_world_development_by_year,
+)
